dc0cc821dd0c90608c0569e6fb2922c8bb2cc200
Clinton Halpin <clintonhalpin@gmail.com>
2017-07-19 19:29:27 -0400

    Update Config

--- app/util/config.js
+++ app/util/config.js
@@ -1,3 +1,24 @@
 let env = process.env.NODE_ENV || 'development';
 
-module.exports = require('./../../etc/.env' + (env ? '.' + env : '') + '.js');
+if( env === production ) {
+
+	/**
+	 * Heroku Stuff
+	 */
+	
+	module.exports = {
+    protocol: process.env.protocol
+    host: process.env.host
+    port: process.env.port
+    token: process.env.token
+    clientId: process.env.clientId
+    clientSecret: process.env.clientSecret
+	}
+
+} else {
+
+	module.exports = require('./../../etc/.env' + (env ? '.' + env : '') + '.js');
+
+}
+
+
